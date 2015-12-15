c006e5d483e406701ef0b4198392d099560e0061
Igor Pejic <igor.pejic@dr.com>
2015-12-15 13:45:32 +0100

    After v10 lab.

--- v10_template/p01.cpp
+++ v10_template/p01.cpp
@@ -5,13 +5,57 @@
 
 #include <math.h>
 #include <iostream>
+#include <vector>
 
+using namespace std;
+using std::vector;
 
+struct Point
+{
+ Point(float _x, float _y, float _z){x=_x; y=_y; z=_z;}
+ float x, y, z;
+};
+
+vector <Point> pts;
+
+Point calcNormal(Point p1, Point p2, Point p3){
+//v1 = t2 - t1
+//v2 = t3 - t1
+
+//vx = v1y * v2z - v1z * v2y
+//vy = v1z * v2x - v1x * v2z
+//vz = v1x * v2y - v1y * v2x
+	float vx, vy, vz;
+	vx = p2.x-p1.x;
+	vy = p2.y-p1.y;
+	vz = p2.z-p1.z;
+	Point v1(vx, vy, vz);
+
+	vx = p3.x-p1.x;
+	vy = p3.y-p1.y;
+	vz = p3.z-p1.z;
+
+	// normala u drugi smjer
+	//Point v2(-vx, -vy, -vz);
+
+	Point v2(vx, vy, vz);
+
+	vx = v1.y * v2.z - v1.z * v2.y;
+	vy = v1.z * v2.x - v1.x * v2.z;
+	vz = v1.x * v2.y - v1.y * v2.x;
+	float I = sqrt(vx*vx+ vy*vy+ vz*vz);
+	vx /= I;
+	vy /= I;
+	vz /= I;
+	return Point(vx, vy, vz);
+}
+
+// pretpostavka da vektor ima hvatiste u (0,0,0)
 void drawNormal(float p0[3], float p1[3] )
 {
     const float size=1.;
     // draw axis
-    glLineWidth(8);
+    glLineWidth(3);
     glBegin(GL_LINES);
         glColor3f(1, 0, 0);
         glVertex3f(p0[0],p0[1],p0[2]);
@@ -34,6 +78,21 @@ void drawScene()
     float p0[3]={0,0,0};
     float p1[3]={1,1,1};
     drawNormal(p0,p1);
+
+	glRotatef(35, 1, 0, 0);
+    glBegin(GL_TRIANGLES);
+    for(int i=0; i < 3; ++i)
+    {
+	glVertex3f(pts[i].x, pts[i].y, pts[i].z);
+    }
+	float vx, vy, vz;
+
+    Point normal = calcNormal(pts[0], pts[1], pts[2]);
+    glEnd();
+    float n0[3]={0.5,0.2,0};
+    float n1[3] = {normal.x, normal.y, normal.z};
+    drawNormal(n0, n1);
+
     glutSwapBuffers();
     glutPostRedisplay();
 }
@@ -86,6 +145,10 @@ void initRendering()
 
 int main(int argc, char** argv)
 {
+    pts.push_back(Point(0,0,0));
+    pts.push_back(Point(1,0,0));
+    pts.push_back(Point(1,1,0));
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(400, 400);
